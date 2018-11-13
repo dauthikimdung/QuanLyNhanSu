81b73b711638ba193dad3187d98c8e30e73de0ab
SidetIn <43158128+SidetIn@users.noreply.github.com>
2018-11-13 08:35:00 +0700

    Up thieu mat 1 ti

---
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmKhenThuongKiLuat.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace QuanLyNhanSu.GUI
+{
+    partial class FrmKhenThuongKiLuat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode1 = new DevExpress.XtraGrid.GridLevelNode();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmKhenThuongKiLuat));
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboNhanVien = new System.Windows.Forms.ComboBox();
+            this.dgrKTKL = new DevExpress.XtraGrid.GridControl();
+            this.grvKTKL = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.btnDong = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSua = new DevExpress.XtraEditors.SimpleButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnThem = new DevExpress.XtraEditors.SimpleButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpThoiGian = new DevExpress.XtraEditors.DateEdit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbNoiDung = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboKTKL = new System.Windows.Forms.ComboBox();
+            this.btnXoa = new DevExpress.XtraEditors.SimpleButton();
+            this.btnXuat = new DevExpress.XtraEditors.SimpleButton();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrKTKL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvKTKL)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtpThoiGian.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtpThoiGian.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "(Khen thưởng / Kỉ luật)";
+            // 
+            // cboNhanVien
+            // 
+            this.cboNhanVien.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboNhanVien.FormattingEnabled = true;
+            this.cboNhanVien.Location = new System.Drawing.Point(253, 10);
+            this.cboNhanVien.Name = "cboNhanVien";
+            this.cboNhanVien.Size = new System.Drawing.Size(197, 21);
+            this.cboNhanVien.TabIndex = 67;
+            this.cboNhanVien.SelectedValueChanged += new System.EventHandler(this.cboNhanVien_SelectedValueChanged);
+            // 
+            // dgrKTKL
+            // 
+            gridLevelNode1.RelationName = "Level1";
+            this.dgrKTKL.LevelTree.Nodes.AddRange(new DevExpress.XtraGrid.GridLevelNode[] {
+            gridLevelNode1});
+            this.dgrKTKL.Location = new System.Drawing.Point(15, 39);
+            this.dgrKTKL.MainView = this.grvKTKL;
+            this.dgrKTKL.Name = "dgrKTKL";
+            this.dgrKTKL.Size = new System.Drawing.Size(678, 479);
+            this.dgrKTKL.TabIndex = 75;
+            this.dgrKTKL.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.grvKTKL});
+            // 
+            // grvKTKL
+            // 
+            this.grvKTKL.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3});
+            this.grvKTKL.GridControl = this.dgrKTKL;
+            this.grvKTKL.Name = "grvKTKL";
+            this.grvKTKL.OptionsDetail.DetailMode = DevExpress.XtraGrid.Views.Grid.DetailMode.Default;
+            this.grvKTKL.PaintStyleName = "Flat";
+            this.grvKTKL.RowHeight = 30;
+            // 
+            // btnDong
+            // 
+            this.btnDong.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Flat;
+            this.btnDong.Image = ((System.Drawing.Image)(resources.GetObject("btnDong.Image")));
+            this.btnDong.Location = new System.Drawing.Point(724, 475);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(510, 43);
+            this.btnDong.TabIndex = 77;
+            this.btnDong.Text = "Đóng chức năng";
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Flat;
+            this.btnSua.Image = ((System.Drawing.Image)(resources.GetObject("btnSua.Image")));
+            this.btnSua.Location = new System.Drawing.Point(982, 329);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(252, 43);
+            this.btnSua.TabIndex = 80;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 13);
+            this.label3.TabIndex = 81;
+            this.label3.Text = "Thông tin khen thưởng kỉ luật của nhân viên";
+            // 
+            // btnThem
+            // 
+            this.btnThem.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Flat;
+            this.btnThem.Image = ((System.Drawing.Image)(resources.GetObject("btnThem.Image")));
+            this.btnThem.Location = new System.Drawing.Point(724, 329);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(252, 43);
+            this.btnThem.TabIndex = 79;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.dtpThoiGian);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txbNoiDung);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cboKTKL);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(724, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(510, 245);
+            this.groupBox1.TabIndex = 76;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chi tiết khen thưởng kỉ luật";
+            // 
+            // dtpThoiGian
+            // 
+            this.dtpThoiGian.EditValue = new System.DateTime(2017, 7, 3, 22, 39, 47, 97);
+            this.dtpThoiGian.Location = new System.Drawing.Point(175, 42);
+            this.dtpThoiGian.Name = "dtpThoiGian";
+            this.dtpThoiGian.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Flat;
+            this.dtpThoiGian.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dtpThoiGian.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dtpThoiGian.Size = new System.Drawing.Size(164, 22);
+            this.dtpThoiGian.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Thời gian";
+            // 
+            // txbNoiDung
+            // 
+            this.txbNoiDung.Location = new System.Drawing.Point(175, 150);
+            this.txbNoiDung.Multiline = true;
+            this.txbNoiDung.Name = "txbNoiDung";
+            this.txbNoiDung.Size = new System.Drawing.Size(284, 68);
+            this.txbNoiDung.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nội dung";
+            // 
+            // cboKTKL
+            // 
+            this.cboKTKL.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboKTKL.FormattingEnabled = true;
+            this.cboKTKL.Items.AddRange(new object[] {
+            "Kỷ luật",
+            "Khen thưởng"});
+            this.cboKTKL.Location = new System.Drawing.Point(175, 96);
+            this.cboKTKL.Name = "cboKTKL";
+            this.cboKTKL.Size = new System.Drawing.Size(225, 21);
+            this.cboKTKL.TabIndex = 16;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Flat;
+            this.btnXoa.Image = ((System.Drawing.Image)(resources.GetObject("btnXoa.Image")));
+            this.btnXoa.Location = new System.Drawing.Point(724, 378);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(510, 43);
+            this.btnXoa.TabIndex = 78;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnXuat
+            // 
+            this.btnXuat.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Flat;
+            this.btnXuat.Image = ((System.Drawing.Image)(resources.GetObject("btnXuat.Image")));
+            this.btnXuat.Location = new System.Drawing.Point(724, 427);
+            this.btnXuat.Name = "btnXuat";
+            this.btnXuat.Size = new System.Drawing.Size(510, 43);
+            this.btnXuat.TabIndex = 82;
+            this.btnXuat.Text = "Xuất ra file excel";
+            this.btnXuat.Click += new System.EventHandler(this.btnXuat_Click);
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "Mã khen thưởng kỷ luật";
+            this.gridColumn1.FieldName = "KHENTHUONGKYLUATID";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "Hình thức";
+            this.gridColumn2.FieldName = "HINHTHUC";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "Nội dung";
+            this.gridColumn3.FieldName = "NOIDUNG";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 2;
+            // 
+            // FrmKhenThuongKiLuat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 530);
+            this.Controls.Add(this.btnXuat);
+            this.Controls.Add(this.dgrKTKL);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.cboNhanVien);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnXoa);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmKhenThuongKiLuat";
+            this.Text = "FrmKhenThuongKiLuat";
+            ((System.ComponentModel.ISupportInitialize)(this.dgrKTKL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvKTKL)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtpThoiGian.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtpThoiGian.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboNhanVien;
+        private DevExpress.XtraGrid.GridControl dgrKTKL;
+        private DevExpress.XtraGrid.Views.Grid.GridView grvKTKL;
+        private DevExpress.XtraEditors.SimpleButton btnDong;
+        private DevExpress.XtraEditors.SimpleButton btnSua;
+        private System.Windows.Forms.Label label3;
+        private DevExpress.XtraEditors.SimpleButton btnThem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraEditors.SimpleButton btnXoa;
+        private System.Windows.Forms.TextBox txbNoiDung;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboKTKL;
+        private DevExpress.XtraEditors.DateEdit dtpThoiGian;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.SimpleButton btnXuat;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+
+    }
+}
---
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmKhenThuongKiLuat.cs
@@ -0,0 +1,139 @@
+﻿using QuanLyNhanSu.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu.GUI
+{
+    public partial class FrmKhenThuongKiLuat : Form
+    {
+        BindingSource list = new BindingSource();
+        public FrmKhenThuongKiLuat()
+        {
+            InitializeComponent();
+            dtpThoiGian.DateTime = DateTime.Now;
+            dgrKTKL.DataSource = list;
+            LoadCboNhanVien();
+            AddKTKLNhanVien();
+        }
+        void LoadKhenThuongKyLuat(int idnv)
+        {
+            list.DataSource = KhenThuongKyLuatDAO.LoadKhenThuongKyLuat(idnv);
+        }
+        void LoadCboNhanVien()
+        {
+            cboNhanVien.DataSource = NhanVienDAO.LoadCboNhanVien();
+            cboNhanVien.ValueMember = "NHANVIENID";
+            cboNhanVien.DisplayMember = "HOTEN";
+        }
+        void AddKTKLNhanVien()
+        {
+            cboKTKL.DataBindings.Add(new Binding("SelectedIndex", dgrKTKL.DataSource, "HINHTHUC", true, DataSourceUpdateMode.Never));
+            txbNoiDung.DataBindings.Add(new Binding("Text", dgrKTKL.DataSource, "NOIDUNG", true, DataSourceUpdateMode.Never));
+            dtpThoiGian.DataBindings.Add(new Binding("DateTime", dgrKTKL.DataSource, "THOIGIAN", true, DataSourceUpdateMode.Never));
+        }
+
+        private void cboNhanVien_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int idnv = (int)cboNhanVien.SelectedValue;
+                LoadKhenThuongKyLuat(idnv);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idnv = (int)cboNhanVien.SelectedValue;
+                int hinhthuc = cboKTKL.SelectedIndex;
+                string noidung = txbNoiDung.Text;
+                DateTime thoigian = dtpThoiGian.DateTime;
+                if (KhenThuongKyLuatDAO.InsertKTKL(idnv, hinhthuc, noidung, thoigian) == 1)
+                    MessageBox.Show("Thành công");
+                else
+                    MessageBox.Show("Thất bại");
+                LoadKhenThuongKyLuat(idnv);
+            }
+            catch
+            {
+                return;
+            }
+            
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idktkl = Convert.ToInt32(grvKTKL.GetFocusedDataRow()["KHENTHUONGKYLUATID"].ToString());
+                int idnv = (int)cboNhanVien.SelectedValue;
+                int hinhthuc = cboKTKL.SelectedIndex;
+                string noidung = txbNoiDung.Text;
+                DateTime thoigian = dtpThoiGian.DateTime;
+                if (KhenThuongKyLuatDAO.UpdateKTKL(idktkl, idnv, hinhthuc, noidung, thoigian) == 1)
+                    MessageBox.Show("Thành công");
+                else
+                    MessageBox.Show("Thất bại");
+                LoadKhenThuongKyLuat(idnv);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idktkl = Convert.ToInt32(grvKTKL.GetFocusedDataRow()["KHENTHUONGKYLUATID"].ToString());
+                int idnv = (int)cboNhanVien.SelectedValue;
+                if (KhenThuongKyLuatDAO.DeleteKTKL(idktkl) == 1)
+                    MessageBox.Show("Thành công");
+                else
+                    MessageBox.Show("Thất bại");
+                LoadKhenThuongKyLuat(idnv);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void btnXuat_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Excel |*.xls";
+            saveFileDialog1.Title = "Save an Excel File";
+            saveFileDialog1.ShowDialog();
+
+            string FileName = saveFileDialog1.FileName.ToString();
+            try
+            {
+                dgrKTKL.ExportToXls(FileName);
+                MessageBox.Show("Xuất file excel thành công");
+            }
+            catch
+            {
+                MessageBox.Show("Vui lòng đóng file cần ghi lại để quá trình ghi thành công");
+            }
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
